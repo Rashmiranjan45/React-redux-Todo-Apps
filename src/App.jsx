9d8f2968113a2ba43920b17406b4ffd9fa076321
Rashmiranjan Samal <samalrashmi141@gmail.com>
2023-12-18 18:26:10 +0530

    react-redux-project added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import AddTodo from './components/addTodo'
+import Todo from './components/Todo'
+
+function App() {
+
+return (
+    <>
+    <h1
+    style={{backgroundColor:"steelblue",fontSize:"xxx-large"}}
+    >Todo Apps</h1>
+    <AddTodo/>
+    <Todo/>
+    </>
+  )
+}
+
+export default App
